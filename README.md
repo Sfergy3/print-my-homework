58ad7309ad4cae22e74cece7652ad9e6dc21964c
s3 <stanleyl.fergusoniii@gmail.com>
2023-02-09 04:07:49 +0000

    I hope this script prints my assignment to the screen

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # print-my-homework
 i wonder if this new repo will do what i need it to do 
+int
---
+++ helloworld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# print these words
+# Stanley L. Ferguson III
+# 7 Feb 2023
+# print the words fo rthe ops challenge
+
+
+printf "I am working perfectly."
+
+echo " shoutout to cyberciti.biz for the commands"
+
+printf "$END/n"
+
+
+# main
+
+
+
+# end
+
